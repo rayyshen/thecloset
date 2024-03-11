f6c5d48e7cdb1289393695e5c2de348c4ac7eabe
Ray <rayshen071@gmail.com>
2024-03-10 21:55:19 -0400

    menu addition

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -10,7 +10,11 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
     DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
+    DropdownMenuSubTrigger,
+    DropdownMenuSub,
+    DropdownMenuPortal,
+    DropdownMenuSubContent
+} from "@/components/ui/dropdown-menu"
 
 export default function SiteHeader({ children }: PropsWithChildren) {
     return (
@@ -19,12 +23,12 @@ export default function SiteHeader({ children }: PropsWithChildren) {
                 <div className="mr-4 md:flex">
                     <a href="/" className="mr-6 flex items-center space-x-2">
                         <Image
-                        src="/head.png"
-                        height={0}
-                        width={0}
-                        alt="Logo"
-                        sizes="70vw"
-                        style={{ width: '100%', height: 'auto'}}
+                            src="/head.png"
+                            height={0}
+                            width={0}
+                            alt="Logo"
+                            sizes="70vw"
+                            style={{ width: '100%', height: 'auto' }}
                         />
 
                     </a>
@@ -34,31 +38,47 @@ export default function SiteHeader({ children }: PropsWithChildren) {
                 <div className="flex flex-1 items-center justify-end space-x-2">
                     <DropdownMenu>
                         <DropdownMenuTrigger>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12"/><line x1="4" x2="20" y1="6" y2="6"/><line x1="4" x2="20" y1="18" y2="18"/></svg>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-menu"><line x1="4" x2="20" y1="12" y2="12" /><line x1="4" x2="20" y1="6" y2="6" /><line x1="4" x2="20" y1="18" y2="18" /></svg>
                         </DropdownMenuTrigger>
                         <DropdownMenuContent>
+                            <DropdownMenuSub>
+                                <DropdownMenuSubTrigger>
+                                    <Info className="mr-2 h-4 w-4" />
+                                    <span>Who We Are</span>
+                                </DropdownMenuSubTrigger>
+                                <DropdownMenuPortal>
+                                    <DropdownMenuSubContent>
+                                        <DropdownMenuItem>Our Team</DropdownMenuItem>
+                                        <DropdownMenuItem>About the Closet</DropdownMenuItem>
+                                        <DropdownMenuItem>Our Membership <br/>Congregations</DropdownMenuItem>
+                                        <DropdownMenuItem>Our Fundamental <br/>Values and Beliefs</DropdownMenuItem>
+                                        <DropdownMenuItem>Voucher Program</DropdownMenuItem>
+                                        <DropdownMenuItem>Grant Application <br/> Guidelines</DropdownMenuItem>
+                                        <DropdownMenuItem>Scholarships</DropdownMenuItem>
+                                        <DropdownMenuItem>Benefits of Local <br/> Non-Profit Thrift Shops</DropdownMenuItem>
+                                        <DropdownMenuItem>Our Membership <br/> Congregations</DropdownMenuItem>
+                                        <DropdownMenuItem>FAQs</DropdownMenuItem>
+                                    </DropdownMenuSubContent>
+                                </DropdownMenuPortal>
+                            </DropdownMenuSub>
                             <DropdownMenuItem>
-                                <Info className="mr-2 h-4 w-4"/>
-                                <span>Who We Are</span>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <HandHelping className="mr-2 h-4 w-4"/>
+                                <HandHelping className="mr-2 h-4 w-4" />
                                 <span>How to Donate Goods</span>
                             </DropdownMenuItem>
                             <DropdownMenuItem>
-                                <Users className="mr-2 h-4 w-4"/>
+                                <Users className="mr-2 h-4 w-4" />
                                 <span>Volunteering</span>
                             </DropdownMenuItem>
                             <DropdownMenuItem>
-                                <Store className="mr-2 h-4 w-4"/>
+                                <Store className="mr-2 h-4 w-4" />
                                 <span>Treasures at the Closet</span>
                             </DropdownMenuItem>
                             <DropdownMenuItem>
-                                <Briefcase className="mr-2 h-4 w-4"/>
+                                <Briefcase className="mr-2 h-4 w-4" />
                                 <span>Job Openings</span>
                             </DropdownMenuItem>
                             <DropdownMenuItem>
-                                <PhoneCall className="mr-2 h-4 w-4"/>
+                                <PhoneCall className="mr-2 h-4 w-4" />
                                 <span>Contact</span>
                             </DropdownMenuItem>
 
